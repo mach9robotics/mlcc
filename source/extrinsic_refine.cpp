18cd13440f89ed7a1b1097fdf772bd8dea3e3c8e
harveybia <harvey.shi@yahoo.com>
2022-06-27 18:47:35 -0400

    program quit on finish

--- source/extrinsic_refine.cpp
+++ source/extrinsic_refine.cpp
@@ -235,10 +235,5 @@ int main(int argc, char** argv)
     pub_surf.publish(colorCloudMsg);
   }
 
-  ros::Rate loop_rate(1);
-  while(ros::ok())
-  {
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+  return 0;
 }
